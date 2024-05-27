8e75e0fba9f822763e718e8931442bd9d33ce8fc
BEATRIZ APARECIDA DA SILVA <58901049856@info.local>
2024-05-27 11:00:50 -0300

    feat:fazendo formulario

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ function selecionarSlide(indiceSlide) {
 
 // carregamento diamico dos cases
 let listaCases = [
-   
+
 ]
 
 function renderizarCases() {
@@ -138,4 +138,59 @@ function renderizarCases() {
 
     // inserir html dos cases montados no elemento container-cards
     containerCards.innerHTML = template
+}
+
+function carregarCases() {
+    //metodo http get - read/leitura - server para mostrar um item ou uma lista de itens
+    fetch("http://localhost:3000/cases")
+        //deserialization - dessearializacao
+        .then((resposta) => resposta.json())
+        .then((dadostratados) => {
+            console.log(dadostratados)
+            listaCases = dadostratados
+            renderizarCases()
+        })
+}
+
+function solicitarOrcamento(event) {
+    //pegar os valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-texto").value
+
+    //organizar os valores em um objeto 
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    console.log(dadosForm);
+    //enviar a requisicao para a API
+    //metodo HTTP POST - creat/criar -> cadastrar um novo registro (solucoes)
+    fetch("http://localhost:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+        //CASO SUCESSO
+        .then(resposta => {
+            console.log(resposta);
+            //limpar os inputs
+            document.querySelector("#contato form").reset()
+
+            //mostrar alert de sucesso
+            alert("solicitacao enviada com sucesso!!!👍")   
+        })
+.catch(erro =>{
+    console.log(erro)
+     //mostrar alert com mensagem de erro
+    alert("erro na requisição 😢")
+})
+    //CASO ERRO
+   
+
+event.preventDefalt()
 }
